aea16a8da3e94ea83a78261287515940b7339f2b
Andre <andre.oliveirahays@modec.com>
2023-09-07 03:13:53 -0300

    test: adding storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'Boilerplate',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
   <S.Wrapper>
-    <h1>Boilerplate</h1>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
   </S.Wrapper>
 )
 
